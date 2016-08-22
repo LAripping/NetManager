ef04b2120a8d9306b46bf38af545ce949935de18
tsaou <tsaousaras@gmail.com>
2016-08-22 22:24:00 +0300

    First attempt to parse XML

--- wifi/index.php
+++ wifi/index.php
@@ -7,7 +7,7 @@ include($_SERVER['DOCUMENT_ROOT'].'/db_connect.php');
 #Returns an instanciated connection object ( $conn )
 
 
-$content = '<p> Test </br>';
+$content = '<p> UNPREPARED SELECT </br>';
 
 $stmt = "SELECT * FROM packets";
 $result = $conn->query($stmt);
@@ -19,6 +19,26 @@ if ($result == TRUE && $result->num_rows > 0) {
     }
 } else {
     $content .= "0 results";
+}
+
+
+$content .= '</p></br>';
+///////////////////////////////////////////////////
+
+$content = '<p> 5 PACKETS </br>';
+
+$name = $_SERVER['DOCUMENT_ROOT'].'/afull.xml';
+$count = 0;
+
+$file = simplexml_load_file($name);
+foreach ($file->xpath('//packet') as $packet) {
+    if($count==5)   break;
+
+    $content .= $count.$packet."</br></br>";
+	$proto = $packet->proto[4];
+
+    $content .= $proto['name'];
+    $count++;
 }
 
 
