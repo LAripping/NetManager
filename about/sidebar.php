a61aa98eb93bf123b002f6110a065a75bb222da3
LAripping <tsaousaras@gmail.com>
2016-10-19 14:46:43 +0300

    Forgot quote

--- about/sidebar.php
+++ about/sidebar.php
@@ -5,6 +5,6 @@
 */
 $sidebar = '        <li><a href="/about/intro.php">Introduction</a></li>
                     <li><a href="/about/credits.php">Credits</a></li>
-                    <li><a href="/about/version_info.php">Version Info</a></li>
+                    <li><a href="/about/version_info.php">Version Info</a></li>'
 ?>
 
